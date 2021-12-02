9c771fe0bb910772bc7cadae39ffc58d15b13225
tanjoshty <tanjoshty@gmail.com>
2021-12-02 13:35:39 +1100

    create post converted to composition api

--- src/views/CreatePost/CreatePost.ts
+++ src/views/CreatePost/CreatePost.ts
@@ -1,28 +1,60 @@
-import { Vue } from 'vue-class-component';
-import {store} from '@/store';
+import { store } from "@/store";
 import Axios from "axios";
-import router from '@/router';
+import { defineComponent, ref } from 'vue';
+import { useRouter } from "vue-router";
 
-export default class CreatePost extends Vue {
-    public title: string = '';
-    public body: string = '';
+export default defineComponent ({
+    name: 'CreatePost',
+    setup() {
+        const router = useRouter();
 
-    public async createPost(): Promise<void> {
-        const title = this.title;
-        const body = this.body;
+        const title = ref('');
+        const body = ref('');
 
-        try {
-            // API Call to register
-            const response = await Axios.post("/create-post", { title, body, token: store.state.user.token });
-            console.log("Post was successfully created");
-            this.goToSinglePost(response.data);
-            
-        } catch (error) {
-            console.log("There was a problem");
+        const createPost = async(): Promise<void> => {
+            try {
+                const response = await Axios.post("/create-post", { title: title.value, body: body.value, token: store.state.user.token });
+                console.log("Post was successfully created");
+                goToSinglePost(response.data);
+                
+            } catch (error) {
+                console.log("There was a problem");
+            }
+        }
+
+        const goToSinglePost = (id: string): void => {
+            router.push({name: 'singlePost', params: {id}});
+        }
+
+        return {
+            title,
+            body,
+            createPost,
+            goToSinglePost
         }
     }
+})
 
-    public goToSinglePost(id: string): void {
-        router.push({name: 'singlePost', params: {id}});
-    }
-}
+// export default class CreatePost extends Vue {
+//     public title: string = '';
+//     public body: string = '';
+
+//     public async createPost(): Promise<void> {
+//         const title = this.title;
+//         const body = this.body;
+
+//         try {
+//             // API Call to register
+//             const response = await Axios.post("/create-post", { title, body, token: store.state.user.token });
+//             console.log("Post was successfully created");
+//             this.goToSinglePost(response.data);
+            
+//         } catch (error) {
+//             console.log("There was a problem");
+//         }
+//     }
+
+//     public goToSinglePost(id: string): void {
+//         router.push({name: 'singlePost', params: {id}});
+//     }
+// }
